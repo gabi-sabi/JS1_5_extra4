4a0f2132d828cd3148e9fc1946e3adfb21d53314
unknown <gsabolova5@gmail.com>
2024-04-03 19:38:09 +0200

    Add draft of a function skillUpdate

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const updateSkill = ("id", 0) => {
-  const skillProgressHTML = document.querySelector("id");
-  return skillProgressHTML.style.width = "0%";
-};
-  
-  const promptHTML = Number(prompt("Ohodnoťte své znalosti HTML na stupnici od 0 do 100:"));
-  const promptCSS = Number(prompt("Ohodnoťte své znalosti CSS na stupnici od 0 do 100:"));
-  const promptJS = Number(prompt("Ohodnoťte své znalosti JavaScriptu na stupnici od 0 do 100:"));
-
-  const idHTML = "#skill1 .skill__progress";
-  const change = updateSkill(idHTML, promptHTML)
-
-  /* MY VERSION - WITHOUT FUNCTION
   const promptHTML = Number(prompt("Ohodnoťte své znalosti HTML na stupnici od 0 do 100:"));
   const promptCSS = Number(prompt("Ohodnoťte své znalosti CSS na stupnici od 0 do 100:"));
   const promptJS = Number(prompt("Ohodnoťte své znalosti JavaScriptu na stupnici od 0 do 100:"));
@@ -28,4 +15,41 @@ const updateSkill = ("id", 0) => {
   skillProgressCSS.style.width = `${promptCSS}%`;
   skillValueJS.textContent = `${promptJS} / 100`;
   skillProgressJS.style.width = `${promptJS}%`;
-*/
+
+  /*POKUS
+  const updateSkill = (id, number) => {
+    let number = 0;
+    if (number === promptHTML) {
+      let id = "#skill1"
+    } else if (number === promptCSS) {
+      let id = "#skill2"
+    } else if (number === promptJS) {
+      let id = "#skill3"
+    } else {
+      alert("Zkuste zadat hodnoty znovu")
+    }
+
+  const skillProgressHTML = document.querySelector(id);
+  const skillValueHTML = document.querySelector(id);
+  const skillProgressCSS = document.querySelector(id);
+  const skillValueCSS = document.querySelector(id);
+  const skillProgressJS = document.querySelector(id);
+  const skillValueJS = document.querySelector(id);
+
+  skillValueHTML.textContent = number / 100;
+  skillProgressHTML.style.width = number + "%";
+  skillValueCSS.textContent = number / 100;
+  skillProgressCSS.style.width = number + "%";
+  skillValueJS.textContent = number / 100;
+  skillProgressJS.style.width = number + "%";
+  }
+
+    const userInput = (promptHTML, promptCSS, promptJSJS) => {
+    const promptHTML = Number(prompt("Ohodnoťte své znalosti HTML na stupnici od 0 do 100:"));
+    const promptCSS = Number(prompt("Ohodnoťte své znalosti CSS na stupnici od 0 do 100:"));
+    const promptJS = Number(prompt("Ohodnoťte své znalosti JavaScriptu na stupnici od 0 do 100:"));
+   return promptHTML, promptCSS, promptJS; 
+  }
+
+  const userSkill = updateSkill("#skill1", userInput);*/
+
